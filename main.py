98e9dada8c257468928640c3974e669b650ba59b
Gautam2000 <63543004+Gautam-MV@users.noreply.github.com>
2023-03-27 03:49:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ from Python_Files.predictFromModel import prediction
 
 import detectron2
 from detectron2.utils.logger import setup_logger
+import numpy as np
+import cv2
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+
+
 
 from wsgiref import simple_server
 from flask import Flask, request, render_template
